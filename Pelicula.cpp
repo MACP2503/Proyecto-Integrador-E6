a30c03425450c154fb583832732cd36ccc547b74
MACP2503 <74038341+MACP2503@users.noreply.github.com>
2021-06-12 14:15:22 -0500

    Implementación de Película
    Implementación de Película

---
+++ Pelicula.cpp
@@ -0,0 +1,39 @@
+#include "Pelicula.h"
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+//Constructores
+//Constructor por default
+Pelicula::Pelicula(){
+  this -> ID = 0;
+  this -> nombre = "Desconocido";
+  this -> duracion = 0;
+  this -> genero = "Desconocido";
+  this -> calificacion = 0.0;
+}
+
+//Constructor con parámetros
+Pelicula::Pelicula(int I, string nom, int du, string gen){
+  this -> ID = I;
+  this -> nombre = nom;
+  this -> duracion = du;
+  this -> genero = gen;
+  this -> calificacion = 0.0;
+}
+
+//Métodos
+void Pelicula::evaluar(float cal){
+  this -> calificacion = cal;
+}
+
+void Pelicula::mostrar(){
+  cout << "Pelicula: " << nombre << endl;
+  cout << "ID: " << ID << endl;
+  cout << "Duracion: " << duracion <<endl;
+  cout << "Genero: " << genero << endl;
+  cout << "Calificacion: " << calificacion << endl;
+}
+
